6ef856275127530e283520cae02a99f84608d175
ExtendObject <clipcojnh@gmail.com>
2016-02-10 23:49:44 +0000

    Server shutdown messages

---
+++ src/main/java/com/thebubblenetwork/api/global/bubblepackets/messaging/messages/request/ServerShutdownRequest.java
@@ -0,0 +1,26 @@
+package com.thebubblenetwork.api.global.bubblepackets.messaging.messages.request;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.thebubblenetwork.api.global.bubblepackets.messaging.AbstractMessageObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ServerShutdownRequest extends AbstractMessageObject{
+    public ServerShutdownRequest() {
+    }
+
+    public ServerShutdownRequest(byte[] bytes) {
+        super(bytes);
+    }
+
+    @Override
+    public void serialize(ByteArrayDataInput in) {
+
+    }
+
+    @Override
+    public void parse(DataOutputStream out) throws IOException {
+
+    }
+}
