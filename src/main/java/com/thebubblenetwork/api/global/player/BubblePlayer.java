ba7da89dfd537005b8cf8179d01a1ce91c3f3ae8
ExtendObject <clipcojnh@gmail.com>
2016-03-05 13:35:50 +0000

    Scrapping interface

--- src/main/java/com/thebubblenetwork/api/global/player/BubblePlayer.java
+++ src/main/java/com/thebubblenetwork/api/global/player/BubblePlayer.java
@@ -1,10 +1,13 @@
 package com.thebubblenetwork.api.global.player;
 
+import com.google.common.base.Joiner;
+import com.thebubblenetwork.api.global.data.InvalidBaseException;
 import com.thebubblenetwork.api.global.data.PlayerData;
+import com.thebubblenetwork.api.global.plugin.BubbleHubObject;
 import com.thebubblenetwork.api.global.ranks.Rank;
+import de.mickare.xserver.util.ChatColor;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Copyright Statement
@@ -18,67 +21,245 @@ import java.util.UUID;
  * Class information
  * ---------------------
  * Package: com.thebubblenetwork.api.global.player
- * Date-created: 26/01/2016 20:14
+ * Date-created: 26/01/2016 20:19
  * Project: GlobalAPI
  */
-public interface BubblePlayer<T> {
-    PlayerData getData();
+public abstract class BubblePlayer<T>{
+    public static BubblePlayer getObject(UUID u) {
+        return playerObjectMap.get(u);
+    }
 
-    void setData(Map<String, String> data);
+    public static Map<UUID, BubblePlayer> getPlayerObjectMap() {
+        return playerObjectMap;
+    }
 
-    String getName();
+    private static Map<UUID, BubblePlayer> playerObjectMap = new HashMap<>();
+    private UUID u;
+    private PlayerData data;
+    private T player;
 
-    T getPlayer();
+    protected BubblePlayer(UUID u, PlayerData data) {
+        this.u = u;
+        this.data = data;
+    }
 
-    Rank getRank();
+    public UUID getUUID() {
+        return u;
+    }
 
-    void setRank(Rank rank);
+    public PlayerData getData() {
+        return data;
+    }
 
-    Rank[] getSubRanks();
+    public void setData(Map<String, String> data) {
+        getData().getRaw().clear();
+        for (Map.Entry<String, String> e : data.entrySet()) {
+            getData().getRaw().put(e.getKey(), e.getValue());
+        }
+        update();
+    }
 
-    void setSubRanks(Iterable<Rank> subRanks);
+    @SuppressWarnings("unchecked")
+    public T getPlayer() {
+        if (player == null) {
+            player = (T) (BubbleHubObject.getInstance().getPlayer(getUUID()));
+        }
+        return player;
+    }
 
-    void setSubRanks(Rank... subRanks);
+    private String getRankString() throws InvalidBaseException {
+        return getData().getString(PlayerData.MAINRANK);
+    }
 
-    int getTokens();
+    public Rank getRank() {
+        String s;
+        try {
+            s = getRankString();
+        } catch (InvalidBaseException e) {
+            BubbleHubObject.getInstance().logSevere(e.getMessage());
+            return Rank.getDefault();
+        }
+        return Rank.getRank(s);
+    }
 
-    void setTokens(int tokens);
+    public void setRank(Rank rank) {
+        getData().set(PlayerData.MAINRANK, rank.getName());
+        update();
+    }
 
-    Map<String, Integer> getStats(String game);
+    private String[] getSubRanksString() throws InvalidBaseException {
+        return getData().getString(PlayerData.SUBRANKS).split(",");
+    }
 
-    void setStat(String game, String identifier, int id);
+    public Rank[] getSubRanks() {
+        String[] s;
+        try {
+            s = getSubRanksString();
+        } catch (InvalidBaseException e) {
+            return new Rank[0];
+        }
+        List<Rank> ranks = new ArrayList<>();
+        for (String rankname : s) {
+            Rank r = Rank.getRank(rankname);
+            if (r != null) {
+                ranks.add(r);
+            }
+        }
+        return ranks.toArray(new Rank[0]);
+    }
 
-    Map<String, Integer> getKits(String game);
+    public void setSubRanks(Iterable<Rank> subRanks) {
+        setList(PlayerData.SUBRANKS, toStrings(subRanks));
+        update();
+    }
 
-    void setKit(String game, String indentifier, int id);
+    public void setSubRanks(Rank... subRanks) {
+        setSubRanks(Arrays.asList(subRanks));
+    }
 
-    boolean isAuthorized(String permission);
+    public UUID[] getFriends() {
+        try {
+            return getData().getUUIDList(PlayerData.FRIENDSLIST);
+        } catch (InvalidBaseException e) {
+            return new UUID[0];
+        }
+    }
 
-    UUID getUUID();
+    public void setFriends(Iterable<UUID> friends) {
+        setList(PlayerData.FRIENDSLIST, toStrings(friends));
+        update();
+    }
 
-    UUID[] getFriends();
+    public void setFriends(UUID... friends) {
+        setFriends(Arrays.asList(friends));
+    }
 
-    void setFriends(Iterable<UUID> friends);
+    public UUID[] getFriendIncomingRequests() {
+        try {
+            return getData().getUUIDList(PlayerData.FRIENDINCOMINGRQ);
+        } catch (InvalidBaseException e) {
+            return new UUID[0];
+        }
+    }
 
-    void setFriends(UUID... friends);
+    public Map<String, Integer> getStats(String game) {
+        return getData().getMap(PlayerData.STATSBASE, game);
+    }
 
-    UUID[] getFriendIncomingRequests();
+    public Map<String, Integer> getKits(String game) {
+        return getData().getMap(PlayerData.KITBASE, game);
+    }
 
-    void setFriendsIncomingRequests(UUID... friends);
+    public Map<String, Integer> getHubItems() {
+        return getData().getMapRaw(PlayerData.ITEMSBASE);
+    }
 
-    void setFriendsIncomingRequests(Iterable<UUID> friends);
+    public Map<String, Integer> getPacks() {
+        return getData().getMapRaw(PlayerData.PACKS);
+    }
 
-    Map<String, Integer> getHubItems();
+    public Map<String, Integer> getCurrency() {
+        return getData().getMapRaw(PlayerData.CURRENCYBASE);
+    }
 
-    void setHubItem(String item, int id);
+    public int getTokens() {
+        Map<String, Integer> currency = getCurrency();
+        return currency.containsKey(PlayerData.TOKENS) ? currency.get(PlayerData.TOKENS) : 0;
+    }
 
-    Map<String, Integer> getPacks();
+    public void setTokens(int tokens) {
+        getData().set(PlayerData.CURRENCYBASE + "." + PlayerData.TOKENS, tokens);
+        update();
+    }
 
-    void setPacks(String pack, int amount);
+    public int getKeys(){
+        Map<String, Integer> currency = getCurrency();
+        return currency.containsKey(PlayerData.KEYS) ? currency.get(PlayerData.KEYS) : 0;
+    }
 
-    String getNickName();
+    public void setKeys(int keys) {
+        getData().set(PlayerData.CURRENCYBASE + "." + PlayerData.KEYS, keys);
+        update();
+    }
 
-    void setNick(String nick);
+    public boolean isAuthorized(String permission) {
+        return getRank().isAuthorized(permission);
+    }
 
-    void save();
+    public void setStat(String game, String indentifier, int id) {
+        getData().set(PlayerData.STATSBASE + "." + game + "." + indentifier, id);
+        update();
+    }
+
+    public void setKit(String game, String indentifier, int id) {
+        getData().set(PlayerData.KITBASE + "." + game + "." + indentifier, id);
+        update();
+    }
+
+    public void setFriendsIncomingRequests(UUID... friends) {
+        setFriendsIncomingRequests(Arrays.asList(friends));
+    }
+
+    public void setFriendsIncomingRequests(Iterable<UUID> friends) {
+        setList(PlayerData.FRIENDINCOMINGRQ, toStrings(friends));
+        update();
+    }
+
+    private Set<String> toStrings(Iterable<?> objects) {
+        Set<String> set = new HashSet<>();
+        for (Object o : objects) {
+            set.add(o.toString());
+        }
+        return set;
+    }
+
+    private void setList(String base, Iterable<String> friends) {
+        getData().set(base, Joiner.on(",").join(friends));
+    }
+
+    public void setHubItem(String item, int id) {
+        getData().set(PlayerData.ITEMSBASE + ".item", id);
+        update();
+    }
+
+    public void setPacks(String pack, int amount) {
+        getData().set(PlayerData.PACKS + "." + pack, amount);
+        update();
+    }
+
+    public void setNick(String nick) {
+        getData().set(PlayerData.NICKNAME, nick);
+        update();
+    }
+
+    public String getNickName() {
+        String nick;
+        try {
+            nick = getData().getString(PlayerData.NICKNAME);
+        } catch (InvalidBaseException e) {
+            return getName();
+        }
+        return ChatColor.translateAlternateColorCodes('&', nick);
+    }
+
+    //Defaults true
+    public boolean isUsingGadgets() {
+        try {
+            return getData().getBoolean(PlayerData.GADGETS);
+        } catch (InvalidBaseException e) {
+            return true;
+        }
+    }
+
+    public void setUsingGadgets(boolean usingGadgets){
+        getData().set(PlayerData.GADGETS,usingGadgets);
+        update();
+    }
+
+    public abstract String getName();
+
+
+    public void update() {
+
+    }
 }
