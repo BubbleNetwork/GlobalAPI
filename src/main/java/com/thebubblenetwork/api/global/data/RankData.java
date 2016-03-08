1bd620ed8793551dee2bb87457399a16c0d7342c
Eric Hocking <xaoseric@users.noreply.github.com>
2016-03-08 09:46:22 -0500

    add static table string so we can reference it

--- src/main/java/com/thebubblenetwork/api/global/data/RankData.java
+++ src/main/java/com/thebubblenetwork/api/global/data/RankData.java
@@ -7,7 +7,9 @@ import java.util.Map;
  */
 public class RankData extends DataObject {
     public static final String PREFIX = "prefix", SUFFIX = "suffix", INHERITANCE = "inherit";
-
+    
+    public static String table = "ranks";
+    
     public RankData(Map<String, String> data) {
         super(data);
     }
