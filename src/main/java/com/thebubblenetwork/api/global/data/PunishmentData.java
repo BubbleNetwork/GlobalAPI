6899777d9f8cc3d1d2138367cc7130ee410ff9bf
Eric <ehock7229@outlook.com>
2016-04-01 16:04:53 -0400

    cork on punishment system

--- src/main/java/com/thebubblenetwork/api/global/data/PunishmentData.java
+++ src/main/java/com/thebubblenetwork/api/global/data/PunishmentData.java
@@ -1,15 +1,30 @@
 package com.thebubblenetwork.api.global.data;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class PunishmentData extends DataObject {
 
-    public static final String BAN = "ban", MUTE = "mute", TEMPBAN = "tempban", TEMPMUTE = "tempmute", IPBAN = "ipban", REASON = "reason";
+    public static final String BANNED = "ban.banned", BANTIME = "ban.time", BANREASON = "ban.reason", BANBY = "ban.by", MUTED = "mute.muted", MUTETIME = "mute.time", MUTEREASON = "mute.reason", MUTEBY = "mute.by";
 
     public static String table = "punishments";
 
     public PunishmentData(Map<String, String> data) {
         super(data);
+    }
+
+    public UUID[] getUUIDList(String indentifier) throws InvalidBaseException {
+        String[] list = getString(indentifier).split(",");
+        Set<UUID> uuids = new HashSet<>();
+        for (String s : list) {
+            try {
+                uuids.add(UUID.fromString(s));
+            } catch (Exception ex) {
+            }
+        }
+        return uuids.toArray(new UUID[uuids.size()]);
     }
 
 }
