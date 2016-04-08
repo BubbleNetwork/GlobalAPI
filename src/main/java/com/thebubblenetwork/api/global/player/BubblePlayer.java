59e5e5ae0809318190344af14c4e202fccbd2681
ExtendObject <clipcojnh@gmail.com>
2016-04-08 10:50:44 +0100

    Moving punishment to bungeecord

--- src/main/java/com/thebubblenetwork/api/global/player/BubblePlayer.java
+++ src/main/java/com/thebubblenetwork/api/global/player/BubblePlayer.java
@@ -40,11 +40,10 @@ public abstract class BubblePlayer<T> {
     private static Map<UUID, BubblePlayer> playerObjectMap = new HashMap<>();
     private UUID u;
     private PlayerData data;
-    private PunishmentData punishmentData;
     private T player;
     private NamelessUser user;
 
-    protected BubblePlayer(UUID u, Map<String,String> rawdata, Map<String, String> rawpunishmentData) {
+    protected BubblePlayer(UUID u, Map<String,String> rawdata) {
         this.u = u;
         data = new PlayerData(rawdata);
         user = new NamelessUser(BubbleHub.getInstance().getNameless(), u);
@@ -54,9 +53,6 @@ public abstract class BubblePlayer<T> {
         catch (InvalidBaseException ex){
             setRank(Rank.getDefault());
         }
-        punishmentData = new PunishmentData(rawpunishmentData) {
-            protected void changed() { BubblePlayer.this.finishChanges(); }
-        };
 
     }
 
@@ -68,21 +64,10 @@ public abstract class BubblePlayer<T> {
         return data;
     }
 
-    public PunishmentData getPunishmentData() {
-        return punishmentData;
-    }
-
     public void setData(Map<String, String> data) {
         getData().getRaw().clear();
         for (Map.Entry<String, String> e : data.entrySet()) {
             getData().getRaw().put(e.getKey(), e.getValue());
-        }
-    }
-
-    public void setPunishmentData(Map<String, String> rawPunishmentData) {
-        getPunishmentData().getRaw().clear();
-        for (Map.Entry<String, String> e : rawPunishmentData.entrySet()) {
-            getPunishmentData().getRaw().put(e.getKey(), e.getValue());
         }
     }
 
@@ -299,118 +284,6 @@ public abstract class BubblePlayer<T> {
     public void setUsingGadgets(boolean usingGadgets) {
         getData().set(PlayerData.GADGETS, usingGadgets);
         finishChanges();
-    }
-
-    public void unban(){
-        getPunishmentData().remove(PunishmentData.BANNED);
-        getPunishmentData().remove(PunishmentData.BANTIME);
-        getPunishmentData().remove(PunishmentData.BANREASON);
-        getPunishmentData().remove(PunishmentData.BANBY);
-        finishChanges();
-    }
-
-    public void ban(Date unbanby, String reason, String by){
-        getPunishmentData().set(PunishmentData.BANNED, true);
-        getPunishmentData().set(PunishmentData.BANTIME, unbanby.getTime());
-        getPunishmentData().set(PunishmentData.BANREASON, reason);
-        getPunishmentData().set(PunishmentData.BANBY, by);
-        finishChanges();
-    }
-
-    public boolean isBanned(){
-        try{
-            if(getPunishmentData().getBoolean(PunishmentData.BANNED)){
-                if(new Date(getPunishmentData().getNumber(PunishmentData.BANTIME).longValue()).before(new Date())){
-                    unban();
-                }
-                else return true;
-            }
-        }
-        catch (InvalidBaseException ex){
-        }
-        return false;
-    }
-
-    public String getBanReason(){
-        try{
-            return getPunishmentData().getString(PunishmentData.BANREASON);
-        }
-        catch (InvalidBaseException e){
-            return null;
-        }
-    }
-
-    public String getBannedBy(){
-        try{
-            return getPunishmentData().getString(PunishmentData.BANBY);
-        }
-        catch (InvalidBaseException e){
-            return null;
-        }
-    }
-
-    public Date getUnbanDate(){
-        try {
-            return new Date(getPunishmentData().getNumber(PunishmentData.BANTIME).longValue());
-        } catch (InvalidBaseException e) {
-            return null;
-        }
-    }
-
-    public void mute(Date unmuteby, String reason, String by) {
-        getPunishmentData().set(PunishmentData.MUTED, true);
-        getPunishmentData().set(PunishmentData.MUTETIME, unmuteby.getTime());
-        getPunishmentData().set(PunishmentData.MUTEREASON, reason);
-        getPunishmentData().set(PunishmentData.MUTEBY, by);
-        finishChanges();
-    }
-
-    public void unmute() {
-        getPunishmentData().remove(PunishmentData.MUTED);
-        getPunishmentData().remove(PunishmentData.MUTETIME);
-        getPunishmentData().remove(PunishmentData.MUTEREASON);
-        getPunishmentData().remove(PunishmentData.MUTEBY);
-        finishChanges();
-    }
-
-    public boolean isMuted() {
-        try{
-            if(getPunishmentData().getBoolean(PunishmentData.MUTED)){
-                if(new Date(getPunishmentData().getNumber(PunishmentData.MUTETIME).longValue()).before(new Date())){
-                    unban();
-                }
-                else return true;
-            }
-        }
-        catch (InvalidBaseException ex){
-        }
-        return false;
-    }
-
-    public String getMuteReason(){
-        try{
-            return getPunishmentData().getString(PunishmentData.MUTEREASON);
-        }
-        catch (InvalidBaseException e){
-            return null;
-        }
-    }
-
-    public String getMutedBy(){
-        try{
-            return getPunishmentData().getString(PunishmentData.MUTEBY);
-        }
-        catch (InvalidBaseException e){
-            return null;
-        }
-    }
-
-    public Date getUnmuteDate(){
-        try {
-            return new Date(getPunishmentData().getNumber(PunishmentData.MUTETIME).longValue());
-        } catch (InvalidBaseException e) {
-            return null;
-        }
     }
 
     public boolean isSpectating(){
