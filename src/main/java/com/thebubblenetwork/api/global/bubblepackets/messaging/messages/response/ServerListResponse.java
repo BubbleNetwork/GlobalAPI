bc133e7429bc446388b8237e36dde2cb0d05b652
ExtendObject <clipcojnh@gmail.com>
2016-04-13 16:36:04 +0100

    New ServerListResponse

--- src/main/java/com/thebubblenetwork/api/global/bubblepackets/messaging/messages/response/ServerListResponse.java
+++ src/main/java/com/thebubblenetwork/api/global/bubblepackets/messaging/messages/response/ServerListResponse.java
@@ -1,30 +1,59 @@
 package com.thebubblenetwork.api.global.bubblepackets.messaging.messages.response;
 
 import com.google.common.io.ByteArrayDataInput;
-import com.thebubblenetwork.api.global.bubblepackets.messaging.messages.AbstractDataMapMessageObject;
+import com.thebubblenetwork.api.global.bubblepackets.messaging.AbstractMessageObject;
 import com.thebubblenetwork.api.global.type.ServerType;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ServerListResponse extends AbstractDataMapMessageObject {
+public class ServerListResponse extends AbstractMessageObject {
+    public static EncapsulatedServer createServer(int id, int playercount, boolean connect){
+        EncapsulatedServer server = new EncapsulatedServer();
+        server.id = id;
+        server.playercount = playercount;
+        server.connect = connect;
+        return server;
+    }
+
     private String servertype;
+    private List<EncapsulatedServer> serverList;
 
-    public ServerListResponse(Map<String, String> data, ServerType servertype) {
-        super(data);
+    public ServerListResponse(ServerType servertype, List<EncapsulatedServer> serverList) {
         this.servertype = servertype.getName();
+        this.serverList = serverList;
     }
 
-    public void serializeInfo(ByteArrayDataInput in) {
+    public void serialize(ByteArrayDataInput in) {
         servertype = in.readUTF();
+        serverList = new ArrayList<>();
+        int size = in.readInt();
+        for(int current = 0;current < size; current++){
+            EncapsulatedServer server = new EncapsulatedServer();
+            server.id = in.readInt();
+            server.playercount = in.readInt();
+            server.connect = in.readBoolean();
+        }
     }
 
-    public void parseInfo(DataOutputStream out) throws IOException {
+    public void parse(DataOutputStream out) throws IOException {
         out.writeUTF(servertype);
+        out.write(serverList.size());
+        for(EncapsulatedServer server: serverList){
+            out.writeInt(server.id);
+            out.writeInt(server.playercount);
+            out.writeBoolean(server.connect);
+        }
     }
 
     public ServerType getServertype() {
         return ServerType.getType(servertype);
+    }
+
+    static class EncapsulatedServer{
+        int id, playercount;
+        boolean connect;
     }
 }
